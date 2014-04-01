04d90e372ea7baa86d2ed91bf0c02f39ab183ce6
Wojtek <krzaczekw@gmail.com>
2014-04-01 19:51:37 +0200

    permutations done

---
+++ TravelingSalesman/src/com/pwr/Combinator.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (c) 2014 Nokia Solutions and Networks. All rights reserved.
+ */
+package com.pwr;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author: Wojciech Krzaczek (wojciech.krzaczek@nsn.com)
+ */
+public class Combinator {
+
+	private List<String> combinations;
+	private String end;
+
+	public Combinator() {
+		combinations = new ArrayList<>();
+	}
+
+	public List<String> getCombinations(List<String> cities) {
+		String begin = cities.get(0);
+		end = cities.get(cities.size() - 1);
+		cities.remove(0);
+		cities.remove(cities.size() - 1);
+
+		createPermutations(cities, begin);
+		return combinations;
+	}
+
+	private void createPermutations(List<String> cities, String begin) {
+		if (cities.isEmpty()) {
+			if (!combinations.contains(begin + end)) {
+				combinations.add(begin + end);
+			}
+		} else {
+			for (String city : cities) {
+				List<String> citiesLeft = new ArrayList<>(cities);
+				citiesLeft.remove(city);
+				createPermutations(citiesLeft, begin + city);
+			}
+		}
+
+	}
+}
